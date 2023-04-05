39da9206e69fa9b2975611ce18b74b0cff6920e0
윤우혁 <squareyun@gmail.com>
2023-04-05 11:36:09 +0900

    [D4] Title: [Professional] 키 순서, Time: 961 ms, Memory: 91,528 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -24,42 +24,32 @@ public class Solution {
 				b = Integer.parseInt(st.nextToken());
 				adj[a][b] = 1; // 유향
 			}
-
-			int ans = 0;
-			for (int k = 1; k <= N; k++) {
-				cnt = 0;
-				gtDFS(k, new boolean[N + 1]);
-				ltDFS(k, new boolean[N + 1]);
-				if (cnt == N - 1) {
-					ans++;
+			
+			for (int k = 1; k <=N; k++) { // 경유(학생k)
+				for (int i = 1; i <= N; i++) { // 출발(학생i)
+					if (i == k || adj[i][k] == 0) continue; // 경유효과 없음. (알수 있는 관계 없음), i와 k 관계가 없어 경유 불가
+					for (int j = 1; j <= N; j++) { // 도착(학생j)
+						// i<k<j : 학생 i보다 학생 k가 키가 크고, 학생 k보다 학생 j가 키가 크면 학생 i보다 학생 j의 키가 크다.
+						if (adj[i][j] == 1) continue; // 이미 i보다 j가 크다는 사실을 알면 패스
+						adj[i][j] = adj[k][j];
+					}
 				}
 			}
+			
+			int ans = 0;
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					adj[i][0] += adj[i][j]; // 자신보다 키가 큰 학생수 누적
+					adj[0][j] += adj[i][j]; // 자신보다 키가 작은 학생수 누적
+				}
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				if (adj[i][0] + adj[0][i] == N - 1)
+					++ans;
+			}
+			
 			System.out.println("#" + tc + " " + ans);
 		}
 	}
-
-	// 자신보다 큰 애를 따라서 DFS 수행
-	static void gtDFS(int cur, boolean[] visited) {
-		// cur 정점 기준으로 자신보다 큰 정점 탐색
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (adj[cur][i] == 1 && !visited[i]) {
-				cnt++;
-				gtDFS(i, visited);
-			}
-		}
-	}
-
-	// 자신보다 작은애 애를 따라서 DFS 수행
-	static void ltDFS(int cur, boolean[] visited) {
-		// cur 정점 기준으로 자신보다 큰 정점 탐색
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (adj[i][cur] == 1 && !visited[i]) {
-				cnt++;
-				ltDFS(i, visited);
-			}
-		}
-	}
-
 }
