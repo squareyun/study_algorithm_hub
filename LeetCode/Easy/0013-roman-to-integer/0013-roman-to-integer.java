6ff5c1d85237e6a92974b7b3d19221619325b9ae
윤우혁 <squareyun@gmail.com>
2024-03-30 21:55:59 +0900

    Time: 7 ms (13.60%), Space: 44.4 MB (88.66%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -5,16 +5,15 @@ import java.util.*;
 class Solution {
     public int romanToInt(String s) {
         
-        HashMap<String, Integer> dict = makeDict();
+        HashMap<Character, Integer> dict = makeDict();
         int len = s.length();
         int answer = 0;
         
         for (int i=0; i<len; i++) {
-            if (i != len - 1 && dict.containsKey(s.substring(i, i+2))) {
-                answer += dict.get(s.substring(i, i+2));
-                i++;
+            if (i != len - 1 && dict.get(s.charAt(i)) < dict.get(s.charAt(i+1))) {
+                answer -= dict.get(s.charAt(i));
             } else {
-                answer += dict.get(s.substring(i, i+1));
+                answer += dict.get(s.charAt(i));
             }
         }
         
@@ -22,20 +21,16 @@ class Solution {
     }
     
     static HashMap makeDict() {
-        HashMap<String, Integer> dict = new HashMap<>();
-        dict.put("I", 1);
-        dict.put("V", 5);
-        dict.put("X", 10);
-        dict.put("L", 50);
-        dict.put("C", 100);
-        dict.put("D", 500);
-        dict.put("M", 1000);
-        dict.put("IV", 4);
-        dict.put("IX", 9);
-        dict.put("XL", 40);
-        dict.put("XC", 90);
-        dict.put("CD", 400);
-        dict.put("CM", 900);
+        HashMap<Character, Integer> dict = new HashMap<>();
+        
+        dict.put('I', 1);
+        dict.put('V', 5);
+        dict.put('X', 10);
+        dict.put('L', 50);
+        dict.put('C', 100);
+        dict.put('D', 500);
+        dict.put('M', 1000);
+        
         return dict;
     }
 }
