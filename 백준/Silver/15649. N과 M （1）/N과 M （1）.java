c91b6e413a68ff23febb871f8c5c23b13797c94b
윤우혁 <squareyun@gmail.com>
2024-06-07 16:48:55 +0900

    [Silver III] Title: N과 M (1), Time: 364 ms, Memory: 25508 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,46 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    static int N, M;
-    static int[] arr, brr;
-    static boolean[] v;
-    static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] arr, brr;
+	static boolean[] v;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[N];
-        for (int i = 1; i <= N; i++) {
-            arr[i - 1] = i;
-        }
-        brr = new int[M];
-        v = new boolean[N];
-        dfs(0);
-        System.out.println(sb);
-    }
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		brr = new int[N];
+		v = new boolean[N];
 
-    static void dfs(int cur) {
-        if (cur == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(brr[i]).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
 
-        for (int i = 0; i < N; i++) {
-            if (v[i]) {
-                continue;
-            }
+		dfs(0);
+		System.out.println(sb);
+	}
 
-            v[i] = true;
-            brr[cur] = arr[i];
-            dfs(cur + 1);
-            v[i] = false;
-        }
-    }
+	static void dfs(int cnt) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(brr[i]).append(' ');
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (v[i]) continue;
+
+			v[i] = true;
+			brr[cnt] = arr[i];
+			dfs(cnt + 1);
+			v[i] = false;
+		}
+	}
 }
