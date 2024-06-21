9acae3ca10b5208f32249012e20aeb7e5d2fd9b0
윤우혁 <squareyun@gmail.com>
2024-06-21 22:10:35 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] dp = new int[N + 2][10];
+
+		for (int i = 0; i < 10; i++) {
+			dp[1][i] = 1;
+		}
+
+		for (int i = 2; i <= N + 1; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = j; k < 10; k++) {
+					dp[i][j] += dp[i - 1][k];
+					dp[i][j] %= 10007;
+				}
+				if (i == (N + 1) && j == 0)
+					break;
+			}
+		}
+
+		System.out.println(dp[N + 1][0]);
+	}
+}
