5e385ed436787eee4f1e51998c6c14f389fd593b
윤우혁 <squareyun@gmail.com>
2024-06-26 12:23:24 +0900

    [Bronze II] Title: N! mod P (1), Time: 1604 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.java
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.java
@@ -10,10 +10,21 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
+		System.out.println(solve(N, P));
+	}
+
+	static long solve(int N, int P) {
+		if (N < P)
+			return factorialMod(N, P);
+		else
+			return factorialMod(P - 1, P);
+	}
+
+	static long factorialMod(int K, int P) {
 		long answer = 1;
-		for (int i = 2; i <= N; i++) {
+		for (int i = 2; i <= K; i++) {
 			answer = (answer * i) % P;
 		}
-		System.out.println(answer);
+		return answer;
 	}
 }
