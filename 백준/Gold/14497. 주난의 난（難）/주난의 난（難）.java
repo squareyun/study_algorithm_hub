48e357df085c3a372e7709f13c4db2de6be90961
윤우혁 <squareyun@gmail.com>
2024-07-04 00:42:56 +0900

    [Gold IV] Title: 주난의 난(難), Time: 172 ms, Memory: 20092 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,18 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	static int N, M;
-	static int sx, sy, ex, ey;
 	static char[][] map;
-	static int[][] dist;
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0, -1};
+	static Data s, e;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,66 +15,57 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
+		s = new Data(0, 0, 0);
 		st = new StringTokenizer(br.readLine());
-		sx = Integer.parseInt(st.nextToken()) - 1;
-		sy = Integer.parseInt(st.nextToken()) - 1;
-		ex = Integer.parseInt(st.nextToken()) - 1;
-		ey = Integer.parseInt(st.nextToken()) - 1;
+		s.x = Integer.parseInt(st.nextToken()) - 1;
+		s.y = Integer.parseInt(st.nextToken()) - 1;
+
+		e = new Data(0, 0, 0);
+		e.x = Integer.parseInt(st.nextToken()) - 1;
+		e.y = Integer.parseInt(st.nextToken()) - 1;
 
 		map = new char[N][M];
-		dist = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			map[i] = br.readLine().toCharArray();
-			Arrays.fill(dist[i], Integer.MAX_VALUE);
 		}
-		map[sx][sy] = '0';
-		map[ex][ey] = '1';
 
-		dijkstra();
-	}
+		ArrayDeque<Data> q = new ArrayDeque<>();
+		boolean[][] v = new boolean[N][M];
+		q.offer(s);
+		v[s.x][s.y] = true;
 
-	static void dijkstra() {
-		PriorityQueue<Data> pq = new PriorityQueue<Data>();
-		pq.offer(new Data(sx, sy, 0));
-		dist[sx][sy] = 0;
+		while (!q.isEmpty()) {
+			Data cur = q.poll();
 
-		while (!pq.isEmpty()) {
-			Data cur = pq.poll();
-
-			if (cur.x == ex && cur.y == ey) {
-				System.out.println(dist[ex][ey]);
+			if (cur.x == e.x && cur.y == e.y) {
+				System.out.println(cur.cnt);
 				return;
 			}
-
-			if (cur.w > dist[cur.x][cur.y]) continue;
 
 			for (int d = 0; d < 4; d++) {
 				int nx = cur.x + dx[d];
 				int ny = cur.y + dy[d];
 
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+				if (v[nx][ny]) continue;
+				v[nx][ny] = true;
 
-				if (dist[nx][ny] > dist[cur.x][cur.y] + (map[nx][ny] - '0')) {
-					dist[nx][ny] = dist[cur.x][cur.y] + (map[nx][ny] - '0');
-					pq.offer(new Data(nx, ny, dist[nx][ny]));
+				if (map[nx][ny] == '0') {
+					q.addFirst(new Data(nx, ny, cur.cnt));
+				} else {
+					q.addLast(new Data(nx, ny, cur.cnt + 1));
 				}
 			}
 		}
-
 	}
 
-	static class Data implements Comparable<Data> {
-		int x, y, w;
+	static class Data {
+		int x, y, cnt;
 
-		public Data(int x, int y, int w) {
+		public Data(int x, int y, int cnt) {
 			this.x = x;
 			this.y = y;
-			this.w = w;
-		}
-
-		@Override
-		public int compareTo(Data o) {
-			return Integer.compare(this.w, o.w);
+			this.cnt = cnt;
 		}
 	}
 }
