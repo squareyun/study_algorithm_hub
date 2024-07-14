733cfbcc31c241e4c90a37425da370d62bb92f6f
윤우혁 <squareyun@gmail.com>
2024-07-15 01:56:42 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 616 ms, Memory: 68888 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -4,13 +4,13 @@ import java.io.*;
 public class Main {
 
 	static int N, E;
-	static List<Data>[] adj;
-	static int must1, must2;
-	static int[] dp;
-	static final int INF = 987654321;
-	static int answer = INF;
+	static List<Edge>[] adj;
+	static int v1, v2;
+
+	private static final int INF = 987654321;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
@@ -19,74 +19,69 @@ public class Main {
 		for (int i = 0; i <= N; i++) {
 			adj[i] = new ArrayList<>();
 		}
+
+		int from, to, w;
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			adj[a].add(new Data(b, w));
-			adj[b].add(new Data(a, w));
+			from = Integer.parseInt(st.nextToken());
+			to = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			adj[from].add(new Edge(to, w));
+			adj[to].add(new Edge(from, w));
 		}
 		st = new StringTokenizer(br.readLine());
-		must1 = Integer.parseInt(st.nextToken());
-		must2 = Integer.parseInt(st.nextToken());
+		v1 = Integer.parseInt(st.nextToken());
+		v2 = Integer.parseInt(st.nextToken());
 
-		int a, b, c, res1, res2;
-		// 1 -> mx -> my -> N
-		a = dijkstra(1, must1);
-		b = dijkstra(must1, must2);
-		c = dijkstra(must2, N);
-		if (a == INF || b == INF || c == INF)
-			res1 = INF;
-		else
-			res1 = a + b + c;
-
-		// 1 -> my -> mx -> N
-		a = dijkstra(1, must2);
-		b = dijkstra(must2, must1);
-		c = dijkstra(must1, N);
-		if (a == INF || b == INF || c == INF)
-			res2 = INF;
-		else
-			res2 = a + b + c;
-
-		answer = ((res1 == INF || res2 == INF) ? -1 : Math.min(res1, res2));
-		System.out.println(answer);
+		int dist1 = solve(v1, v2);
+		int dist2 = solve(v2, v1);
+		int answer = Math.min(dist1, dist2);
+		System.out.println((answer == INF) ? -1 : answer);
 	}
 
-	static int dijkstra(int s, int e) {
-		dp = new int[N + 1];
+	private static int solve(int first, int second) {
+		int a = dijkstra(1, first);
+		if (a == INF) return INF;
+		int b = dijkstra(first, second);
+		if (b == INF) return INF;
+		int c = dijkstra(second, N);
+		if (c == INF) return INF;
+		return a + b + c;
+	}
+
+	static int dijkstra(int start, int end) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		int[] dp = new int[N + 1];
 		Arrays.fill(dp, INF);
-		PriorityQueue<Data> pq = new PriorityQueue<>();
-		pq.offer(new Data(s, 0));
-		dp[s] = 0;
+		dp[start] = 0;
+		pq.offer(new Edge(start, 0));
 
 		while (!pq.isEmpty()) {
-			Data cur = pq.poll();
+			Edge cur = pq.poll();
 
 			if (cur.w > dp[cur.v]) continue;
 
-			for (Data next : adj[cur.v]) {
+			for (Edge next : adj[cur.v]) {
 				if (dp[next.v] > dp[cur.v] + next.w) {
 					dp[next.v] = dp[cur.v] + next.w;
-					pq.offer(new Data(next.v, dp[next.v]));
+					pq.offer(new Edge(next.v, dp[next.v]));
 				}
 			}
 		}
 
-		return dp[e];
+		return dp[end];
 	}
 
-	static class Data implements Comparable<Data> {
+	static class Edge implements Comparable<Edge> {
 		int v, w;
 
-		public Data(int v, int w) {
+		public Edge(int v, int w) {
 			this.v = v;
 			this.w = w;
 		}
 
 		@Override
-		public int compareTo(Data o) {
+		public int compareTo(Edge o) {
 			return Integer.compare(this.w, o.w);
 		}
 	}
