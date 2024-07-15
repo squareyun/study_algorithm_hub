29d746f192e447f0819d9bdc098fcb5060ef2901
윤우혁 <squareyun@gmail.com>
2024-07-15 19:16:25 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 24748 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,31 +4,34 @@ import java.io.*;
 public class Main {
 
 	static int N, S;
-	static int[] arr;
+	static long[] cum;
+	static int answer = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
-		arr = new int[N + 1];
-
+		cum = new long[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			cum[i] = cum[i - 1] + Integer.parseInt(st.nextToken());
 		}
 
-		int s = 0, e = 0;
-		int sum = 0;
-		int len = Integer.MAX_VALUE;
-		while (s <= e && e <= N) {
+		// System.out.println(Arrays.toString(cum));
+		int left = 0, right = 1;
+		while (right <= N) {
+			long sum = cum[right] - cum[left];
+
 			if (sum >= S) {
-				len = Math.min(len, e - s);
-				sum -= arr[s++];
+				answer = Math.min(answer, right - left);
+				left++;
 			} else {
-				sum += arr[e++];
+				right++;
 			}
 		}
-		System.out.println((len == Integer.MAX_VALUE) ? 0 : len);
+
+		System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
 	}
 }
