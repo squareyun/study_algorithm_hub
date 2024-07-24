4e4f12307a7d3611612542c02f20dde10f1e7e2b
윤우혁 <squareyun@gmail.com>
2024-07-24 10:15:11 +0900

    [Gold IV] Title: 점프, Time: 248 ms, Memory: 23600 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.java
+++ 백준/Gold/2253. 점프/점프.java
@@ -4,10 +4,10 @@ import java.io.*;
 public class Main {
 
 	static int N, M;
-	static HashSet<Integer> hs = new HashSet<>();
-	static HashSet<Integer>[] v;
+	static HashSet<Integer> smallRocks;
+	static int[][] dp;
 
-	static final int INF = 987654321;
+	final static int INF = 987654321;
 
 	public static void main(String[] args) throws IOException {
 
@@ -15,47 +15,29 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
+		smallRocks = new HashSet<>();
 		if (M != 0) {
 			for (int i = 0; i < M; i++) {
-				hs.add(Integer.parseInt(br.readLine()));
+				smallRocks.add(Integer.parseInt(br.readLine()));
 			}
 		}
 
-		v = new HashSet[N + 1];
+		dp = new int[N + 1][(int)(Math.sqrt(2 * N)) + 2];
 		for (int i = 0; i <= N; i++) {
-			v[i] = new HashSet<>();
+			Arrays.fill(dp[i], INF);
 		}
+		dp[1][0] = 0;
 
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[]{1, 0, 0});
-		int answer = INF;
-		int[] dx = {-1, 0, 1};
-		while (!q.isEmpty()) {
-			int[] data = q.poll();
-			int cur = data[0];
-			int x = data[1];
-			int cnt = data[2];
+		for (int i = 1; i <= N; i++) {
+			if (smallRocks.contains(i)) continue;
 
-			// System.out.println(cur + " " + x + " " + cnt);
-
-			for (int d = 0; d < 3; d++) {
-				int nextX = x + dx[d];
-
-				if (nextX < 1) continue;
-
-				if (cur + nextX == N) {
-					answer = Math.min(answer, cnt + 1);
-					break;
-				}
-
-				int next = cur + nextX;
-				if (next > N || hs.contains(next) || v[next].contains(nextX))
-					continue;
-				q.offer(new int[] {next, nextX, cnt + 1});
-				v[next].add(nextX);
+			for (int j = 1; j < (int)(Math.sqrt(2 * i)) + 1; j++) {
+				dp[i][j] = Math.min(dp[i - j][j - 1], Math.min(dp[i - j][j], dp[i - j][j + 1])) + 1;
 			}
-
+		}
+		int answer = INF;
+		for (int i = 1; i < dp[N].length; i++) {
+			answer = Math.min(answer, dp[N][i]);
 		}
 		System.out.println(answer == INF ? -1 : answer);
 	}
