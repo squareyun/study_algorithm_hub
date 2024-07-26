d152540bd3e155e460a54fc4744336b7a7dfb7fd
윤우혁 <squareyun@gmail.com>
2024-07-26 14:39:46 +0900

    [Gold IV] Title: RGB거리 2, Time: 136 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -4,51 +4,45 @@ import java.io.*;
 public class Main {
 
 	static int N;
-	static int[][] arr;
-	static int[][] dp;
-	static int[] answer;
+	static int[][] arr, dp;
+
+	static final int INF = 987654321;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N][3];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		arr = new int[N + 1][3];
+		StringTokenizer st;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 
-		dp = new int[N][3];
-		answer = new int[3];
-
-		for (int k = 0; k < 3; k++) { // 첫 집에 k 색 칠하기
-			for (int i = 0; i < 3; i++) {
-				if (i == k)
-					dp[0][i] = arr[0][i];
-				else
-					dp[0][i] = 1001;
+		int answer = INF;
+		for (int i = 0; i < 3; i++) {
+			dp = new int[N + 1][3];
+			for (int j = 0; j < 3; j++) {
+				if (i == j) dp[1][j] = arr[1][j];
+				else dp[1][j] = INF;
 			}
 
-			for (int i = 1; i < N; i++) {
-				dp[i][0] = arr[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-				dp[i][1] = arr[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-				dp[i][2] = arr[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+			for (int j = 2; j <= N; j++) {
+				dp[j][0] = arr[j][0] + Math.min(dp[j - 1][1], dp[j - 1][2]);
+				dp[j][1] = arr[j][1] + Math.min(dp[j - 1][0], dp[j - 1][2]);
+				dp[j][2] = arr[j][2] + Math.min(dp[j - 1][0], dp[j - 1][1]);
 			}
 
-			if (k == 0)
-				answer[0] = Math.min(dp[N - 1][1], dp[N - 1][2]);
-			if (k == 1)
-				answer[1] = Math.min(dp[N - 1][0], dp[N - 1][2]);
-			if (k == 2)
-				answer[2] = Math.min(dp[N - 1][0], dp[N - 1][1]);
+			for (int j = 0; j < 3; j++) {
+				if (i == j) dp[N][j] = INF;
+			}
 
-
-			// for (int i = 0; i < N; i++) {
-			// 	System.out.println(Arrays.toString(dp[i]));
-			// }
-			// System.out.println("=======");
+			for (int j = 0; j < 3; j++) {
+				answer = Math.min(answer, dp[N][j]);
+			}
 		}
-
-		// System.out.println(Arrays.toString(answer));
-		System.out.println(Arrays.stream(answer).min().getAsInt());
+		System.out.println(answer);
 	}
 }
