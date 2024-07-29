a73a516763aa7d52bd145a0aea745b7c3c14b6af
윤우혁 <squareyun@gmail.com>
2024-07-29 23:56:56 +0900

    [Gold I] Title: K번째 수, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -4,8 +4,6 @@ import java.io.*;
 public class Main {
 
 	static int N, K;
-	static int[][] A;
-	static int[] B;
 
 	public static void main(String[] args) throws IOException {
 
@@ -13,27 +11,22 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		K = Integer.parseInt(br.readLine());
 
-		// B[K] = x 에 대해, x의 값을 역으로 추정하는 이분 탐색
-		long left = 1;
-		long right = K; // x <= K
-		long answer = 0;
+		long lo = 1, hi = K;
+		while (lo + 1 < hi) {
+			long mid = (lo + hi) / 2;
 
-		while (left <= right) {
-			long mid = (left + right) / 2;
 			long cnt = 0;
-
 			for (int i = 1; i <= N; i++) {
 				cnt += Math.min(N, mid / i);
 			}
 
-			if (cnt < K) {
-				left = mid + 1;
+			if (!(K <= cnt)) {
+				lo = mid;
 			} else {
-				answer = mid;
-				right = mid - 1;
+				hi = mid;
 			}
 		}
-
-		System.out.println(answer);
+		System.out.println(hi);
 	}
+
 }
