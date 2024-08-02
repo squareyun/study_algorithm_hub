65a5847be0b0f734b6b5163addf617d1f4699aeb
윤우혁 <squareyun@gmail.com>
2024-08-02 13:06:40 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 528 ms, Memory: 96136 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Main {
 
-	static int N, idx;
-	static int[] A, dp;
+	static int N;
+	static int[] A;
 
 	public static void main(String[] args) throws IOException {
 
@@ -15,29 +15,25 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-		dp = new int[N + 1];
 
-		idx = 0;
+		int[] res = new int[N + 1];
+		int p = 0;
 		for (int i = 0; i < N; i++) {
-			if (dp[idx] < A[i]) {
-				dp[++idx] = A[i];
+			if (res[p] < A[i]) {
+				res[++p] = A[i];
 			} else {
-				int pos = lowerBound(0, idx, A[i]);
-				dp[pos] = A[i];
+				int lo = 0, hi = p;
+				while (lo + 1 < hi) {
+					int mid = (lo + hi) / 2;
+
+					if (!(A[i] <= res[mid])) lo = mid;
+					else hi = mid;
+				}
+				res[hi] = A[i];
 			}
 		}
-		System.out.println(idx);
+		// System.out.println(Arrays.toString(res));
+		System.out.println(p);
 	}
 
-	private static int lowerBound(int lo, int hi, int K) {
-		while (lo + 1 < hi) {
-			int mid = (lo + hi) / 2;
-
-			if (!(K <= dp[mid]))
-				lo = mid;
-			else
-				hi = mid;
-		}
-		return hi;
-	}
 }
