cf2b73aff0ddde7740c198012f610f2131030681
윤우혁 <squareyun@gmail.com>
2024-08-11 11:01:21 +0900

    [Silver V] Title: 카드 색칠, Time: 524 ms, Memory: 80788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24912. 카드 색칠/카드 색칠.java
+++ 백준/Silver/24912. 카드 색칠/카드 색칠.java
@@ -10,62 +10,32 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
+		arr = new int[N + 2];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		System.out.println(solve());
-	}
-
-	static String solve() {
-		if (N == 1) {
-			if (arr[0] != 0)
-				return String.valueOf(arr[0]);
-			else
-				return "1";
-		}
-
-		if (arr[0] == 0) {
-			arr[0] = (arr[1] == 1 ? 2 : 1);
-		}
-		for (int i = 0; i < N; i++) {
-			if (i == 0) {
-				if (arr[i] == arr[i + 1])
-					return "-1";
-				continue;
-			} else if (i == N - 1) {
-				if (arr[i] == arr[i - 1])
-					return " -1;";
-				continue;
-			}
-
-			if (arr[i] != 0 && arr[i - 1] != 0 && arr[i + 1] != 0) {
-				if (arr[i] == arr[i - 1])
-					return "-1";
-				continue;
-			}
-
+		for (int i = 1; i <= N; i++) {
 			if (arr[i] == 0) {
-				if (arr[i - 1] == 1) {
-					arr[i] = (arr[i + 1] == 2 ? 3 : 2);
-				} else if (arr[i - 1] == 2) {
-					arr[i] = (arr[i + 1] == 1 ? 3 : 1);
-				} else if (arr[i - 1] == 3) {
-					arr[i] = (arr[i + 1] == 1 ? 2 : 1);
+				for (int j = 1; j <= 3; j++) {
+					if (arr[i - 1] != j && arr[i + 1] != j) {
+						arr[i] = j;
+						break;
+					}
+				}
+			} else {
+				if (arr[i] == arr[i + 1]) {
+					System.out.println(-1);
+					return;
 				}
 			}
 		}
 
-		if (arr[N - 1] == 0) {
-			arr[N - 1] = (arr[N - 2] == 1 ? 2 : 1);
-		}
-
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			sb.append(arr[i]).append(" ");
 		}
-		return sb.toString();
+		System.out.println(sb);
 	}
 }
