35b61d79cbda0e1ce27a963775193d97e57ab3d4
윤우혁 <squareyun@gmail.com>
2024-08-12 16:40:54 +0900

    [Silver I] Title: 암호화된 비밀번호, Time: 380 ms, Memory: 68160 KB -BaekjoonHub

--- 백준/Silver/9549. 암호화된 비밀번호/암호화된 비밀번호.java
+++ 백준/Silver/9549. 암호화된 비밀번호/암호화된 비밀번호.java
@@ -11,39 +11,49 @@ public class Main {
 		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		while (T-- > 0) {
-			String encrypted = br.readLine();
+			String encrypt = br.readLine();
 			String origin = br.readLine();
-			boolean result = solve(origin, encrypted);
-			sb.append(result ? "YES" : "NO").append("\n");
+
+			int[] originAlpha = new int[26];
+			for (char c : origin.toCharArray()) {
+				originAlpha[c - 'a']++;
+			}
+
+			int[] encryptAlpha = new int[26];
+			for (char c : encrypt.substring(0, origin.length()).toCharArray()) {
+				encryptAlpha[c - 'a']++;
+			}
+
+			boolean find = false;
+			for (int i = 0; i <= encrypt.length() - origin.length(); i++) {
+				if (i == 0) {
+					if (check(originAlpha, encryptAlpha)) {
+						find = true;
+						break;
+					}
+					continue;
+				}
+
+				encryptAlpha[encrypt.charAt(i - 1) - 'a']--;
+				encryptAlpha[encrypt.charAt(i + origin.length() - 1) - 'a']++;
+				if (check(originAlpha, encryptAlpha)) {
+					find = true;
+					break;
+				}
+			}
+
+			if (find) sb.append("YES");
+			else sb.append("NO");
+			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static boolean solve(String origin, String encrypted) {
-
-		int lenOrigin = origin.length();
-		int lenEncrypted = encrypted.length();
-
-		int[] cntOrigin = new int[26];
-		for (int i = 0; i < lenOrigin; i++) {
-			cntOrigin[origin.charAt(i) - 'a']++;
+	private static boolean check(int[] originAlpha, int[] encryptAlpha) {
+		for (int i = 0; i < originAlpha.length; i++) {
+			if (originAlpha[i] != encryptAlpha[i]) return false;
 		}
-
-		int[] cntEncrypted = new int[26];
-		for (int i = 0; i < lenOrigin; i++) {
-			cntEncrypted[encrypted.charAt(i) - 'a']++;
-		}
-
-		if (Arrays.equals(cntOrigin, cntEncrypted))
-			return true;
-
-		for (int i = lenOrigin; i < lenEncrypted; i++) {
-			cntEncrypted[encrypted.charAt(i) - 'a']++;
-			cntEncrypted[encrypted.charAt(i - lenOrigin) - 'a']--;
-			if (Arrays.equals(cntOrigin, cntEncrypted))
-				return true;
-		}
-
-		return false;
+		return true;
 	}
+
 }
