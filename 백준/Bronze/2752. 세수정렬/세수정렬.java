6603de7b67a42c3b73c6b746a6bae8e040aec467
윤우혁 <squareyun@gmail.com>
2024-09-28 15:50:27 +0900

    [Bronze IV] Title: 세수정렬, Time: 116 ms, Memory: 16044 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[3];
+		for (int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < 3; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
