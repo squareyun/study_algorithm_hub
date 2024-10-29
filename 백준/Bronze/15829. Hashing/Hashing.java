aa2875fbd59c7c58f65c3448a52ff180b1a91dd0
윤우혁 <squareyun@gmail.com>
2024-10-29 22:19:56 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14264 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,10 +8,12 @@ public class Main {
         int L = Integer.parseInt(br.readLine());
         String s = br.readLine();
         long H = 0L;
+        long pow = 1L;
         for (int i = 0; i < L; i++) {
-            H += (s.charAt(i) - 'a' + 1) * (long) Math.pow(31, i);
+            H += (s.charAt(i) - 'a' + 1) * pow;
+            pow = (pow * 31) % 1234567891;
         }
-        System.out.println(H);
+        System.out.println(H % 1234567891);
     }
 
 }
