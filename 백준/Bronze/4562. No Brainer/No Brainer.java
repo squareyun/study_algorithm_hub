3dbdf955c6ea3741e4ec4f05d7104f4943b56ec8
윤우혁 <squareyun@gmail.com>
2024-11-23 02:30:59 +0900

    [Bronze IV] Title: No Brainer, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x < y) sb.append("NO BRAINS");
+            else sb.append("MMM BRAINS");
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
