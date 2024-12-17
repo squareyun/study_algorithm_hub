393d7b379a08d23f06543caf7cc4297dd981d4ed
윤우혁 <squareyun@gmail.com>
2024-12-17 20:41:31 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
