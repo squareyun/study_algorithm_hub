0792e8211dfd7411705c6edb3c0e11bb436c3c73
윤우혁 <squareyun@gmail.com>
2024-12-30 10:08:14 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A + B).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
