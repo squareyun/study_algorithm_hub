5714ec1a532404598c1c7cb181caf10e4b705e0e
윤우혁 <squareyun@gmail.com>
2025-01-10 19:40:13 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            arr[Integer.parseInt(br.readLine())] = true;
+        }
+        for (int i = 1; i <= 30; i++) {
+            if (!arr[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
