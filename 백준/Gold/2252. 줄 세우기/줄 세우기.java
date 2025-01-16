64cdcc041880bbfb029d6395994c73a5e42724e4
윤우혁 <squareyun@gmail.com>
2025-01-16 20:48:07 +0900

    [Gold III] Title: 줄 세우기, Time: 1924 ms, Memory: 116500 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,62 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int N, M;
-	static ArrayList<Integer>[] list;
-	static int[] inDegree;
+    static int N, M;
+    static ArrayList<Integer>[] adj;
+    static int[] inDegree;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[N + 1];
+        inDegree = new int[N + 1];
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		list = new ArrayList[N + 1];
-		inDegree = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
 
-		for (int i = 0; i < N + 1; i++)
-			list[i] = new ArrayList<>();
+        int a, b;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            adj[a].add(b);
+            inDegree[b]++;
+        }
 
-		int a, b;
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			inDegree[b]++;
-		}
+        ArrayList<Integer> ret = topologySort();
+        for (Integer r : ret) {
+            System.out.printf("%d ", r);
+        }
+    }
 
-		ArrayList<Integer> ret = topologySort();
-		for (Integer r : ret) {
-			System.out.printf("%d ", r);
-		}
-	}
+    private static ArrayList<Integer> topologySort() {
+        ArrayList<Integer> ret = new ArrayList<>();
+        Queue<Integer> q = new ArrayDeque<>();
 
-	private static ArrayList<Integer> topologySort() {
-		ArrayList<Integer> ret = new ArrayList<>();
-		Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0)
+                q.offer(i);
+        }
 
-		for (int i = 1; i <= N; i++) {
-			if (inDegree[i] == 0)
-				q.offer(i);
-		}
+        while (!q.isEmpty()) {
+            Integer cur = q.poll();
+            ret.add(cur);
+            for (int next : adj[cur]) {
+                if (--inDegree[next] == 0)
+                    q.offer(next);
+            }
+        }
 
-		Integer cur;
-		while (!q.isEmpty()) {
-			cur = q.poll();
-			ret.add(cur);
-			for (int idx : list[cur]) {
-				if (--inDegree[idx] == 0)
-					q.offer(idx);
-			}
-		}
-		return ret;
-	}
+        return ret;
+    }
 }
