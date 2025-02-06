24a6fee25250c1d12649548727fc348cf496b63c
윤우혁 <squareyun@gmail.com>
2025-02-06 23:48:56 +0900

    [Bronze IV] Title: 특식 배부, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(N, A) + Math.min(N, B) + Math.min(N, C));
+    }
+
+}
