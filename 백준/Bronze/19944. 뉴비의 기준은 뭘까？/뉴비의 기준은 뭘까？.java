71f775c247456c6c570f1b8eb05c5d77d246b2cd
윤우혁 <squareyun@gmail.com>
2025-02-15 14:25:40 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        if (M == 1 || M == 2) {
+            System.out.println("NEWBIE!");
+        } else if (M <= N) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+    }
+
+}
