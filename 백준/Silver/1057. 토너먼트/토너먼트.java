33df5b9208718606dc814b5359b2263254447bae
윤우혁 <squareyun@gmail.com>
2025-02-27 18:42:34 +0900

    [Silver IV] Title: 토너먼트, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while (A != B) {
+            A = A / 2 + A % 2;
+            B = B / 2+ B % 2;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+}
