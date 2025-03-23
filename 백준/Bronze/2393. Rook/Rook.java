6da9c99e1c54e0e38b58b68b20a5293d1fce6462
윤우혁 <squareyun@gmail.com>
2025-03-24 02:58:55 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
