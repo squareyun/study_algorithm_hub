a1e9f99f59cce78b8627c977361f04d80256c17b
윤우혁 <squareyun@gmail.com>
2025-03-27 19:22:45 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 100 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append(" ");
+            if (i % 6 == 0)
+                sb.append("Go! ");
+        }
+        if (N % 6 != 0)
+            sb.append("Go!");
+        System.out.println(sb);
+    }
+
+}
