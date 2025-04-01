a1006ec9a6862c2e4179221ab566574bba6fa30a
윤우혁 <squareyun@gmail.com>
2025-04-01 18:12:01 +0900

    [Gold V] Title: 빗물, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -15,24 +15,23 @@ public class Main {
 			blocks[i] = Integer.parseInt(st.nextToken());
 		}
 
+		int[] lMax = new int[W];
+		int[] rMax = new int[W];
+
+		lMax[0] = blocks[0];
+		for (int i = 1; i < W; i++) {
+			lMax[i] = Math.max(lMax[i - 1], blocks[i]);
+		}
+
+		rMax[W - 1] = blocks[W - 1];
+		for (int i = W - 2; i >= 0; i--) {
+			rMax[i] = Math.max(rMax[i + 1], blocks[i]);
+		}
+
 		int ans = 0;
-
-		for (int i = 1; i < W - 1; i++) {
-			int lMax = 0;
-			int rMax = 0;
-
-			for (int j = 0; j < i; j++) {
-				lMax = Math.max(lMax, blocks[j]);
-			}
-
-			for (int j = i + 1; j < W; j++) {
-				rMax = Math.max(rMax, blocks[j]);
-			}
-
-			int minH = Math.min(lMax, rMax);
-			if (minH > blocks[i]) {
-				ans += minH - blocks[i];
-			}
+		for (int i = 0; i < W; i++) {
+			int minH = Math.min(lMax[i], rMax[i]);
+			ans += minH - blocks[i];
 		}
 
 		System.out.println(ans);
