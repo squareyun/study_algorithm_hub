c63c9b3be6087c943a7c43e63976c286be66f9ee
윤우혁 <squareyun@gmail.com>
2025-04-06 00:55:16 +0900

    [Bronze V] Title: Plane, Time: 112 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int k1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int k2 = Integer.parseInt(st.nextToken());
+
+        int totalSeats = n1 * k1 + n2 * k2;
+
+        System.out.println(totalSeats);
+    }
+
+}
