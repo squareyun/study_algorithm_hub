1f1d651aa1212bc1be36705aab958c3811ca84c8
윤우혁 <squareyun@gmail.com>
2025-05-09 20:09:50 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 112 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int U = 2 * M - N;
+            int intact = M - U;
+            System.out.println(U + " " + intact);
+        }
+    }
+}
