ed02f8ca9a1fd96b9f3d2d6198c83083997947dd
윤우혁 <squareyun@gmail.com>
2025-05-30 00:02:19 +0900

    [Bronze III] Title: 종이접기, Time: 484 ms, Memory: 72228 KB -BaekjoonHub

---
+++ 백준/Bronze/16504. 종이접기/종이접기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                sum += Long.parseLong(st.nextToken());
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
